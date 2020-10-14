7b095d23f24f48b24c354de5ec6bc74cdabebb4e
Mehmet Sökmen <sokmen4083@gmail.com>
2020-10-15 01:06:02 +0200

    Express Router

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 const express = require('express'),
-const http = require('http');
+      http = require('http');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
+const dishRouter = require('./routes/dishRouter');
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
 app.use(morgan('dev'));
 app.use(bodyParser.json());
+
+app.use('/dishes', dishRouter);
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+  console.log(req.headers);
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<html><body><h1>This is an Express Server</h1></body></html>');
+
+});
 
 app.all('/dishes', (req,res,next) => {
   res.statusCode = 200;
@@ -52,15 +64,7 @@ app.delete('/dishes/:dishId', (req, res, next) => {
     res.end('Deleting dish: ' + req.params.dishId);
 });
 
-app.use(express.static(__dirname + '/public'));
 
-app.use((req, res, next) => {
-  console.log(req.headers);
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<html><body><h1>This is an Express Server</h1></body></html>');
-
-});
 
 const server = http.createServer(app);
 
